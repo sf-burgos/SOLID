4bfd8c80fff14ef62c4006c6efd792305e4c8292
sf-burgos <brayan.burgosdelgado@kinesso.com>
2022-09-19 23:28:51 -0500

    O-Open_Closed

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class AreaCalculator {
 
-  private double computeAreaCalculator (List<Rectangule> rectangles) {
-    return rectangles.stream().mapToDouble(rectangle -> rectangle.getHeight()*rectangle.getWidth().doubleValue()).sum();
+  private double computeAreaCalculator(List<Rectangule> rectangles) {
+    return rectangles.stream().mapToDouble(rectangle -> rectangle.getHeight() * rectangle.getWidth()).sum();
   }
 }
